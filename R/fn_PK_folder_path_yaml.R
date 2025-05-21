2928c6c4fc043d31043d5794820e77d4e8e5bffb
deliaspanigo <david.elias949@gmail.com>
2025-05-21 12:06:40 +0200

    path

--- R/fn_PK_folder_path_yaml.R
+++ R/fn_PK_folder_path_yaml.R
@@ -13,18 +13,28 @@ fn_PK_folder_path_yaml <- function(){
   # ----------------------------------------------------------------------------
   # if(!dir.exists(the_local_folder)){
     #From packages
-    the_package_path <- find.package("Rscience.menu")
-    the_folder_path <- file.path(the_package_path, "yaml") # NO DETALLAR "inst"!!!!
+  
+    # the_package_path <- find.package("Rscience.menu")
+    # the_folder_path <- file.path(the_package_path, "yaml") # NO DETALLAR "inst"!!!!
     
   # }
   # ----------------------------------------------------------------------------
   
+    the_package <- "Rscience.menu"
+    the_folder <- "yaml"
+    
+    the_package_path <- find.package(the_package)
+    vector_folder_paths <- list.dirs(path = the_package_path, recursive = T)
+    dt_selected_folder <- grepl(the_folder, vector_folder_paths, ignore.case = TRUE)
+    selected_folder_path <- vector_folder_paths[dt_selected_folder]
+    
+    if(length(selected_folder_path)>1) selected_folder_path <- selected_folder_path[1]
+    
+    selected_folder_path    
   
-
   
   
-  
-  return(the_folder_path)
+  return(selected_folder_path)
 }
 
 
