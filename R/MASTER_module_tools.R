e0e7b2ba603758d5965e7bf55695e307f24215fe
deliaspanigo <david.elias949@gmail.com>
2025-06-06 13:11:32 +0200

    new menu

--- R/MASTER_module_tools.R
+++ R/MASTER_module_tools.R
@@ -79,7 +79,9 @@ MASTER_module_tools_server <- function(id, show_dev = FALSE) {
     
     list_df_R <- fn_SUPER_mod_better_df(list_all_yaml)
     
-    vector_tools_OK <- c("GeneralLM_fix_anova1", "GeneralLM_fix_slinreg")
+    vector_tools_OK <- c("GeneralLM_fix_anova1", 
+                         "GeneralLM_fix_slreg")
+    
     names(vector_tools_OK) <- vector_tools_OK
     
     vector_cartel <- c("Anova 1 way - Fixed Effects - General Linear Model",
